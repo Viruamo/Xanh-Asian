91a6d90bbe2a6f70d4b2eb3c71662c38468b8dda
Viruamo <tuanngocduongdinh@gmail.com>
2026-02-13 23:17:44 +0700

    Increase logo sizes in header and footer

--- js/footer.js
+++ js/footer.js
@@ -6,7 +6,7 @@ export function Footer() {
   div.className = 'bg-black text-gray-400 py-12';
   div.innerHTML = `
     <div class="max-w-7xl mx-auto px-4 text-center">
-      <img src="/images/logocty.png" alt="Logo" class="w-10 h-10 mx-auto mb-4" />
+      <img src="/images/logocty.png" alt="Logo" class="w-16 h-16 mx-auto mb-4" />
       <p class="mb-8">${t('copyright')}</p>
     </div>
   `;
